08c3a2e29a3a863851ac23c35194d48fd9103156
Brayan <brayan.loucher@outlook.com>
2022-06-29 14:53:02 +0200

    setup environnement to start the project == setup tailwindcss and extend the variables w mine & setup the useEffect function to call the API

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue-dark-mode':        'hsl(209, 23%, 22%)',
+        'very-dark-blue-dark-mode':   'hsl(207, 26%, 17%)',
+        'very-dark-blue-light-mode':  'hsl(200, 15%, 8%)',
+        'dark-grey-light-mode':       'hsl(0, 0%, 52%)',
+        'very-light-grey-light-mode': 'hsl(0, 0%, 98%)'
+      },
+      fontSize: {
+        'homepage-items':     '14px',
+        'detail-page':        '16px'
+      },
+      screens: {
+        'desktop': '1440px'
+      }
+    },
+  },
+  plugins: [],
+}
